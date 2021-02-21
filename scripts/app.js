91f46a029fd33c6df8420048538576834f496a3c
salpanis <salpanis@gmail.com>
2021-02-21 13:23:23 +0000

    Re-factored code to tidy it a bit

--- scripts/app.js
+++ scripts/app.js
@@ -34,98 +34,10 @@ function init() {
 
   createGrid()
 
-  function addTetromino(tetrominoPosition) {
-    cells[tetrominoPosition].classList.add(tetrominoClass)
-  }
-  function removeTetromino(tetrominoPosition) {
-    cells[tetrominoPosition].classList.remove(tetrominoClass)
-  }
 
-
-  let gravityCount
-  function dropTetromino() {
-    tetrominoPosition = startingPosition
-    gravityCount = 0
-    const dropTimerId = setInterval(() => {
-      console.log('Start button clicked')
-      if (cells[startingPosition].classList.contains(tetrominoClass) || grid.classList.contains('stop-game')) {
-        console.log('Game stopped')
-        clearInterval(dropTimerId)
-        grid.classList.remove('stop-game')
-        cells.forEach(cell => cell.classList.remove(tetrominoClass))
-      } else if (gravityCount <= gridHeight - 1) {
-        addTetromino(tetrominoPosition)
-        removeTetromino(tetrominoPosition)
-        // const currentPosition = tetrominoPosition
-        const nextSpace = tetrominoPosition += gridWidth
-        if ((nextSpace + gridWidth) > cellCount - 1) {
-          addTetromino(nextSpace)
-          cells[nextSpace].classList.add(tetrominoClass)
-          tetrominoPosition = startingPosition
-          gravityCount = 0
-        } else if (cells[nextSpace + gridWidth].classList.contains(tetrominoClass) === true) {
-          addTetromino(nextSpace)
-          cells[nextSpace].classList.add(tetrominoClass)
-          tetrominoPosition = startingPosition
-          gravityCount = 0
-        } else {
-          addTetromino(nextSpace)
-        } 
-        gravityCount++
-        console.log(gravityCount)
-      }
-    },gameSpeed)
-  }
-
-  // ! clearinterval if starting position has tetronimo class
-  
-
-
-  document.addEventListener('keyup', handleKeyUp)
-
-  function handleKeyUp(event) {
-    const key = event.keyCode
-    // * 39 is right. 37 is left
-    if (key === 39 && tetrominoPosition % gridWidth !== gridWidth - 1 && !cells[tetrominoPosition + 1].classList.contains(tetrominoClass) && tetrominoPosition + gridWidth <= cellCount - 1) {
-      removeTetromino(tetrominoPosition)
-      tetrominoPosition++
-    } else if (key === 37 && tetrominoPosition % gridWidth !== 0 && !cells[tetrominoPosition - 1].classList.contains(tetrominoClass) && tetrominoPosition + gridWidth <= cellCount - 1) {
-      removeTetromino(tetrominoPosition)
-      tetrominoPosition --
-    } else {
-      console.log('Invalid key')
-    }
-    addTetromino(tetrominoPosition)
-  }
-
-  // Buttons!
-  const startButton = document.querySelector('#play')
-  console.log('Start button', startButton)
-
-  const stopButton = document.querySelector('#stop')
-  // console.log('stop button', stopButton)
-
-  startButton.addEventListener('click',dropTetromino)
-
-  stopButton.addEventListener('click',stopGrid)
-
-  function stopGrid(){
-    if (window.confirm('Are you sure you want to stop the game? All progress will be lost')) {
-      grid.classList.add('stop-game')
-      console.log('Stop button',grid.classList)
-    }
-  }
-
-  // Shapes!
-  // Array of shapes
-  // each shape an object
-  // Object properties - 
-  // width
-  // height
-  // blank spaces ie not class tetronimo
-  // color
   // ? Rotation https://www.w3schools.com/jsref/prop_style_transform.asp ?
 
+  
   const shapes = []
 
   const next = document.querySelector('.next')
@@ -158,6 +70,7 @@ function init() {
         shapeGrid.appendChild(shapeCell)
       }
       next.appendChild(shapeGrid)
+      console.log(shapeGrid)
     }
   }
 
@@ -178,7 +91,76 @@ function init() {
   shapes.push(revEll)
 
   shapes[Math.floor(Math.random() * shapes.length)].createShape()
-  console.log(shapes)
+
+  function addTetromino(tetrominoPosition) {
+    cells[tetrominoPosition].classList.add(tetrominoClass)
+  }
+  function removeTetromino(tetrominoPosition) {
+    cells[tetrominoPosition].classList.remove(tetrominoClass)
+  }
+
+  let gravityCount
+  function dropTetromino() {
+    tetrominoPosition = startingPosition
+    gravityCount = 0
+    const dropTimerId = setInterval(() => {
+      if (cells[startingPosition].classList.contains(tetrominoClass) || grid.classList.contains('stop-game')) {
+        console.log('Game stopped')
+        clearInterval(dropTimerId)
+        grid.classList.remove('stop-game')
+        cells.forEach(cell => cell.classList.remove(tetrominoClass))
+      } else if (gravityCount < gridHeight) {
+        addTetromino(tetrominoPosition)
+        console.log(tetrominoPosition,gravityCount)
+        removeTetromino(tetrominoPosition)
+        // const currentPosition = tetrominoPosition
+        const nextSpace = tetrominoPosition += gridWidth
+        if ((nextSpace + gridWidth) > cellCount - 1 || cells[nextSpace + gridWidth].classList.contains(tetrominoClass) === true) {
+          addTetromino(nextSpace)
+          tetrominoPosition = startingPosition
+          gravityCount = 0
+        } else {
+          addTetromino(nextSpace)
+        } 
+        gravityCount++
+        console.log(gravityCount)
+      }
+    },gameSpeed)
+  }
+
+  
+
+  document.addEventListener('keyup', handleKeyUp)
+
+  function handleKeyUp(event) {
+    const key = event.keyCode
+    // * 39 is right. 37 is left
+    if (key === 39 && tetrominoPosition % gridWidth !== gridWidth - 1 && !cells[tetrominoPosition + 1].classList.contains(tetrominoClass) && tetrominoPosition + gridWidth <= cellCount - 1) {
+      removeTetromino(tetrominoPosition)
+      tetrominoPosition++
+    } else if (key === 37 && tetrominoPosition % gridWidth !== 0 && !cells[tetrominoPosition - 1].classList.contains(tetrominoClass) && tetrominoPosition + gridWidth <= cellCount - 1) {
+      removeTetromino(tetrominoPosition)
+      tetrominoPosition --
+    } else {
+      console.log('Invalid key')
+    }
+    addTetromino(tetrominoPosition)
+  }
+
+  // Buttons!
+  const startButton = document.querySelector('#play')
+  const stopButton = document.querySelector('#stop')
+  startButton.addEventListener('click',dropTetromino)
+  stopButton.addEventListener('click',stopGrid)
+
+  function stopGrid(){
+    if (window.confirm('Are you sure you want to stop the game? All progress will be lost')) {
+      grid.classList.add('stop-game')
+      console.log('Stop button',grid.classList)
+    }
+  }
+
+  
 
 
 
